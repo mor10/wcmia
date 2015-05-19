e5ab6d3ec2b2e76bc7bd6a7a3b9e5e3e1f8226e0
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 11:29:02 -0400

    Clear out unnecessary footer content.

--- footer.php
+++ footer.php
@@ -10,13 +10,6 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wcmia' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wcmia' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wcmia' ), 'wcmia', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>

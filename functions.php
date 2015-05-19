89e65f8dfb4b6ce8aa16a859f8d7ae4bb15b6950
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 10:53:02 -0400

    Remove optional Custom Header support (functions.php reference + custom-header.php)

--- functions.php
+++ functions.php
@@ -92,10 +92,6 @@ function wcmia_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'wcmia_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-//require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.

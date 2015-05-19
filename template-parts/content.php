8c8b65ad9e976f59530ecb23f5bee4b98c1b0a08
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 14:18:43 -0400

    Wrap everything in a link

--- template-parts/content.php
+++ template-parts/content.php
@@ -6,9 +6,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+		<header class="entry-header">
+			<?php the_title(); ?>
 
-	</header><!-- .entry-header -->
-
+		</header><!-- .entry-header -->
+	</a>
+	
 </article><!-- #post-## -->

5d191622cbdcca6455a9669c75f0e96a05c4cfcf
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 14:23:49 -0400

    Display individual movie posters.
    Also, add dummy avatar and poster image for fallbacks.

--- template-parts/content.php
+++ template-parts/content.php
@@ -7,10 +7,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
-		<header class="entry-header">
-			<?php the_title(); ?>
-
-		</header><!-- .entry-header -->
+		<?php 
+        if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+            the_post_thumbnail('mod-poster');
+        } else {
+            echo '<img src="' . get_template_directory_uri() . '/images/poster-dummy.png">';
+        }
+        ?>
 	</a>
-	
+
 </article><!-- #post-## -->

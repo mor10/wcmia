f25b9e81a7dc5ad63a36cdab9298b6b9f769ad12
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 12:28:56 -0400

    Remove post navigation (previous and next nav at the bottom of the post)

--- single.php
+++ single.php
@@ -14,8 +14,6 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php the_post_navigation(); ?>
-
 		<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->

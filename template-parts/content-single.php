bca42a51041c023393265aa5ae0409b384fbfb07
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 12:45:32 -0400

    Add video player and provisional trailer / movie buttons.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -5,6 +5,26 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	<div class="movie-wrapper">
+		<div class="the-movie embed-container" >			
+			<?php 
+				$video_url = get_field('video_url');
+				$attr = array(
+					'poster'	=> esc_url( get_site_url() . '/video/Posters/' . $video_url . '_p.jpg' ),
+					'src'		=> esc_url( get_site_url() . '/video/Films/' . $video_url . '.mp4' ),
+					'width'		=> '',
+					'fullscreen'=> 'true'
+				);
+				echo wp_video_shortcode( $attr );
+			?>
+			<div class="control">
+				<a href="#" class="movie-trigger">Play Trailer</a> 
+				<a href="#" class="movie-trigger">Play Movie</a>
+			</div><!-- .control -->
+		</div><!-- .embed-container -->
+	</div><!-- .movie-wrapper -->
+
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 

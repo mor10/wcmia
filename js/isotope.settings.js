da293ba444d2e6b5a8929fbdef3451b89a8c9562
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 18:02:54 -0400

    Add filter functionality to Isotope settings and hook it to checkboxes.

--- js/isotope.settings.js
+++ js/isotope.settings.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function ($) {
 
     // Set up key variables
     var $container = $('.movie-index'); // The container that holds all the isotope items
+    var $checkboxes = $('.movie-filter input'); // All the individual filter buttons
 
     // Initiate Isotope
     $container.isotope({
@@ -18,5 +19,18 @@ jQuery(document).ready(function ($) {
     $container.imagesLoaded( function() {
         $container.isotope('layout');
     });
+
+    // Filtering section: This function grabs all selected boxes and combines them into an annd filter
+    $checkboxes.change(function () {
+        var $filters = [];
+        // get checked checkboxes values
+        $checkboxes.filter(':checked').each(function () {
+            $filters.push(this.value);
+        });
+        // ['.red', '.blue'] -> '.red, .blue'
+        $filters = $filters.join('');
+        console.log($filters);
+        $container.isotope({ filter: $filters });
+    }); 
     
 });

17eaa16c801baf976058251e2f622fe90cb7da2e
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 18:00:31 -0400

    Add HTML markup for filter select-boxes to correspond with available terms.

--- index.php
+++ index.php
@@ -14,6 +14,73 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
+		<header class="index-header">
+            <div class="filter-trigger">
+                <a id="show-filters" href="#"><span>Filter</span> Movies <i class="icon icon-keyboard-arrow-down"></i></a>
+            </div>
+            <nav id="filter" class="movie-filter">
+            
+                <div class="filter-blocks">
+                
+                <?php
+                $majors = get_terms('category');
+                $genres = get_terms('genres');
+                $types = get_terms('post_tag');
+                $durations = get_terms('durations');
+
+                // Categories
+                echo '<div class="filter-block">';
+                echo '<h3 class="filter-title">Categories</h3>';
+
+                echo '<div class="option-set" data-filter="genre">';
+                echo '<ul>';
+                if ( !empty( $majors ) && !is_wp_error( $majors ) ){
+                    foreach ( $majors as $major ) {
+                        echo '<li><input type="checkbox" value=".' . $major->slug . '" id="' . $major->slug . '"><label for="' . $major->slug . '">' . $major->name . '</label></li>';
+                   }
+                }
+                echo '</ul>';
+                echo '</div>';
+                echo '</div>';
+
+                // Genres
+                echo '<div class="filter-block">';
+                echo '<h3 class="filter-title">Genres</h3>';
+                echo '<div class="option-set" data-filter="genre">';
+                echo '<ul>';
+                if ( !empty( $genres ) && !is_wp_error( $genres ) ){
+                    foreach ( $genres as $genre ) {
+                        echo '<li><input type="checkbox" value=".' . $genre->slug . '" id="' . $genre->slug . '"><label for="' . $genre->slug . '">' . $genre->name . '</label></li>';
+                   }
+                }
+                echo '</ul>';
+                echo '</div>';
+                echo '</div>';
+
+                // Durations
+                echo '<div class="filter-block">';
+                echo '<h3 class="filter-title">Duration</h3>';
+                echo '<div class="option-set" data-filter="duration">';
+                echo '<ul>';
+                if ( !empty( $durations ) && !is_wp_error( $durations ) ){
+                    foreach ( $durations as $duration ) {
+                        echo '<li><input type="checkbox" value=".' . $duration->slug . '" id="' . $duration->slug . '"><label for="' . $duration->slug . '">' . $duration->name . '</label></li>';
+                   }
+                }
+                echo '</ul>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div><!-- .filter-blocks -->';
+                ?>
+                
+                <div class="filter-sub-nav">
+                    <a class="view-all current" href="#filter=*" >clear filters</a>
+                    <a id="close-filters" href="#">close filters</a>
+                </div>  
+                
+            </nav><!-- .project-filter -->
+        </header><!-- .page-header -->
+
 		<main id="main" class="site-main movie-index" role="main">
 
 		<?php if ( have_posts() ) : ?>

3375a09b5576ac8e6f38fb10934e41bb10531c22
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-19 10:51:34 -0400

    Remove Jetpack support

--- functions.php
+++ functions.php
@@ -111,8 +111,3 @@ require get_template_directory() . '/inc/extras.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-require get_template_directory() . '/inc/jetpack.php';

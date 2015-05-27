ea4521f24a737f38c2ddd4d702852e836348dcfe
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-05-27 15:47:22 -0700

    Create Student Index custom page template

---
+++ page-students.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Student Index
+ *
+ * The template for displaying the Students
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<h1 class="students-header">Students</h1>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
